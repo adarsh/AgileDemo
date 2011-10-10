e6685999435a7a806f3453c5d7c7962e9b7af9c9
Adarsh Pandit <adarshpandit@gmail.com>
2011-10-10 15:43:54 -0400

    Styles for Storefront

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,10 +1,11 @@
 class Product < ActiveRecord::Base
+  default_scope :order => 'title'
+  
   validates :title, :description, :image_url, :presence => true
   validates :price, :numericality => {:greater_than_or_equal_to => 0.01}
   validates :title, :uniqueness => true
   validates :image_url, :format => {
     :with   => %r{\.(gif|jpg|png)$}i,
-    :message => "must be a URL for a GIF, JPG, or PNG image."
-    
-  }
+    :message => "must be a URL for a GIF, JPG, or PNG image."}
+  validates_length_of :title, :minimum => 10, :message => "at least 10 characters"
 end
